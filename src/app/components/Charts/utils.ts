a0d07dcfa50b1e99a76c1c58eb4a254608b7c313
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-03-01 14:44:07 -0500

    add-parameter-type-to-formatDateLabel-function

--- src/app/components/Charts/utils.ts
+++ src/app/components/Charts/utils.ts
@@ -1,3 +1,3 @@
-export const formatDateLabel = (date) => {
+export const formatDateLabel = (date: number) => {
     return new Intl.DateTimeFormat("en", { day: "2-digit", month: "short" }).format(date);
 };
