b0a524e2ea62b5ef61f509a97b67156c606ff139
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-03-05 17:37:20 -0500

    fix-api-endpoint

--- src/app/redux/features/api.ts
+++ src/app/redux/features/api.ts
@@ -54,8 +54,9 @@ export const api = createApi({
       },
     }),
     getTableData: builder.query({
-      query: () => "coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1hr&locale=en"
-    })
+      query: () =>
+        "coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d&locale=en",
+    }),
   }),
 });
 
@@ -64,5 +65,5 @@ export const {
   useGetSearchDataQuery,
   useGetLineChartDataQuery,
   useGetBarChartDataQuery,
-  useGetTableDataQuery
+  useGetTableDataQuery,
 } = api;
