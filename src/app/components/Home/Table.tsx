690d857f1b8f6df1bb1a5fbfa775364d4b238d34
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-03-11 14:01:17 -0400

    move-infinite-scroll-component-outside-of-table

--- src/app/components/Home/Table.tsx
+++ src/app/components/Home/Table.tsx
@@ -12,34 +12,47 @@ const Table = () => {
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Name</th>
-          <th>Price</th>
-          <th>1h%</th>
-          <th>24h%</th>
-          <th>7d%</th>
-          <th>24h Volume / Market Cap</th>
-          <th>Circulating / Total Supply</th>
-          <th>Last 7d</th>
-        </tr>
-      </thead>
-      <tbody>
+    <div>
+      {data ? (
         <InfiniteScroll
           dataLength={data.length}
           loader={<h4>Loading...</h4>}
           hasMore={true}
           next={fetchMoreData}
         >
-          {data?.map((coin) => (
-            <CoinRow key={coin.id} coinData={coin} />
-          ))}
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>1h%</th>
+                <th>24h%</th>
+                <th>7d%</th>
+                <th>24h Volume / Market Cap</th>
+                <th>Circulating / Total Supply</th>
+                <th>Last 7d</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data?.map((coin) => (
+                <CoinRow key={coin.id} coinData={coin} />
+              ))}
+            </tbody>
+          </table>
         </InfiniteScroll>
-      </tbody>
-    </table>
+      ) : null}
+    </div>
   );
 };
 
 export default Table;
+
+{
+  /* <InfiniteScroll
+            dataLength={data.length}
+            loader={<h4>Loading...</h4>}
+            hasMore={true}
+            next={fetchMoreData}
+          ></InfiniteScroll> */
+}
