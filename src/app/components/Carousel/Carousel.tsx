1db51de3c2f810b8bc11f194a947ee5cfffeec75
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-03-12 09:57:09 -0400

    setup-carousel-structure-with-basic-styling

--- src/app/components/Carousel/Carousel.tsx
+++ src/app/components/Carousel/Carousel.tsx
@@ -1,7 +1,27 @@
+import styled from "styled-components";
 import CoinCard from "./CoinCard";
 
+const CarouselContainer = styled.div``;
+
+const HeaderDiv = styled.div``;
+
+const StyledText = styled.div`
+  color: #d1d1d1;
+  font-size: 14px;
+  font-weight: 400;
+`;
+
 const Carousel = () => {
-  return <div><CoinCard/></div>;
+  return (
+    <div>
+      <HeaderDiv>
+        <StyledText>Select the currency to view statistics</StyledText>
+      </HeaderDiv>
+      <CarouselContainer>
+        <CoinCard />
+      </CarouselContainer>
+    </div>
+  );
 };
 
 export default Carousel;
