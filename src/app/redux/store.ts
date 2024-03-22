5a5b3a787f2b09bbe1ff478763dfc0b22f4b26e7
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-03-22 11:25:38 -0400

    add-hasMoreReducer-to-store.ts

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "./features/api";
+import hasMoreReducer from "./features/hasMoreSlice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       [api.reducerPath]: api.reducer,
+      hasMore: hasMoreReducer,
     },
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(api.middleware),
