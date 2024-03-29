3ab91c93ac4a2c7968df35df8a6637cc25eb380d
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-03-29 12:11:02 -0400

    create-currencySlice.ts-and-add-reducer-to-store.ts

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "./features/api";
 import hasMoreReducer from "./features/hasMoreSlice";
+import currencyReducer from "./features/currencySlice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       [api.reducerPath]: api.reducer,
       hasMore: hasMoreReducer,
+      currency: currencyReducer,
     },
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(api.middleware),
