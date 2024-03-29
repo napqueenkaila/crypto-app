2d3c2210470efb163a97a9ad13fc77dbf919c380
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-03-29 13:21:31 -0400

    remove-tailwind-code

--- src/app/coin/[coinId]/page.tsx
+++ src/app/coin/[coinId]/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Coin({ params }: { params: { coinId: string } }) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       {params.coinId}
     </main>
   );
