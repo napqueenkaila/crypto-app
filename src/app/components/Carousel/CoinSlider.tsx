68e084390bee2b4b351871c05929002c85a7ca58
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-03 11:41:26 -0400

    call-useGetCarouselDataQuery-in-CoinSlider.tsx-and-map-over-data-to-return-CoinCard-components

--- src/app/components/Carousel/CoinSlider.tsx
+++ src/app/components/Carousel/CoinSlider.tsx
@@ -2,28 +2,26 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import CoinCard from "./CoinCard";
+import { useGetCarouselDataQuery } from "@/app/redux/features/api";
+import { useAppSelector } from "@/app/redux/hooks";
+import { selectCurrency } from "@/app/redux/features/currencySlice";
 
 const CoinSlider = () => {
+  const { currency } = useAppSelector(selectCurrency);
+  const { data, isSuccess } = useGetCarouselDataQuery(currency);
+
   const settings = {
     dots: false,
     speed: 500,
-    slidesToShow: 5,
+    slidesToShow: 4,
     slidesToScroll: 1,
-    
   };
 
   return (
     <Slider {...settings}>
-      <CoinCard />
-      <CoinCard />
-      <CoinCard />
-      <CoinCard />
-      <CoinCard />
-      <CoinCard />
-      <CoinCard />
-      <CoinCard />
-      <CoinCard />
-      <CoinCard />
+      {isSuccess
+        ? data.map((coin) => <CoinCard key={coin.id} coinData={coin} />)
+        : null}
     </Slider>
   );
 };
