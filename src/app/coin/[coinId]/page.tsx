34cbc82edc1b37a24a09205860ce6dc9357e78ed
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-03 11:09:01 -0400

    edit-styled-components-for-page.tsx

--- src/app/coin/[coinId]/page.tsx
+++ src/app/coin/[coinId]/page.tsx
@@ -10,12 +10,16 @@ import CoinProfit from "../CoinProfit";
 const GridContainer = styled.div`
   margin: 0 auto;
   padding: 20px 0;
-  max-width: 80vw;
+  width: 100%;
+  max-width: 90vw;
   display: grid;
   grid-template-areas:
-    "name profit stats"
-    "description description links";
-    gap: 20px;
+    "name profit stats stats"
+    "homelink profit stats stats"
+    "description description links links";
+  gap: 20px;
+  justify-content: center;
+  place-items: stretch center;
 `;
 
 const CoinName = styled.div`
@@ -25,16 +29,26 @@ const CoinName = styled.div`
   font-weight: 700;
   font-size: 28px;
   border-radius: 12px;
-  padding: 40px 56px;
-  max-width: fit-content;
+  padding: 40px 50px;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap:20px;
+`;
+
+const HomeLink = styled.div`
+  grid-area: homelink;
+  place-self: center ;
+  width: 100%;
 `;
 
 const LinkContainer = styled.div`
   grid-area: links;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
 `;
 
 export default function Coin({ params }: { params: { coinId: string } }) {
@@ -44,15 +58,15 @@ export default function Coin({ params }: { params: { coinId: string } }) {
     <main>
       {isSuccess ? (
         <GridContainer>
-          <div>
-            <CoinName>
-              <Image src={data.image.thumb} height={25} width={25} alt="" />
-              <div>
-                {data.name} ({data.symbol.toUpperCase()})
-              </div>
-            </CoinName>
+          <CoinName>
+            <Image src={data.image.thumb} height={25} width={25} alt="" />
+            <div>
+              {data.name} ({data.symbol.toUpperCase()})
+            </div>
+          </CoinName>
+          <HomeLink>
             <CoinLink link={data.links.homepage[0]} />
-          </div>
+          </HomeLink>
           <CoinProfit marketData={data.market_data} />
           <CoinStats marketData={data.market_data} symbol={data.symbol} />
           <CoinDescription description={data.description["en"]} />
