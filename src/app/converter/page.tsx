83150bbfbd7b81e8f734470048215586d54fa6ed
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-07 11:11:44 -0400

    useState-to-track-selected-coins-to-convert-and-fetch-data-for-SelectWithSearch

--- src/app/converter/page.tsx
+++ src/app/converter/page.tsx
@@ -1,3 +1,48 @@
+"use client";
+import { useState } from "react";
+import styled from "styled-components";
+import { useAppSelector } from "../redux/hooks";
+import { selectCurrency } from "../redux/features/currencySlice";
+import { useGetCarouselDataQuery } from "../redux/features/api";
+import SelectWithSearch from "../components/Converter/SelectWithSearch";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const ConverterValueDiv = styled.div`
+  background-color: #191932;
+  padding: 24px;
+`;
+
 export default function Converter() {
-  return <div>converter</div>;
+  const { currency } = useAppSelector(selectCurrency);
+  const [fromCoin, setfromCoin] = useState("bitcoin");
+  const [toCoin, setToCoin] = useState("ethereum");
+  const { data: options } = useGetCarouselDataQuery(currency);
+
+  const mappedOptions = options?.map((option) => {
+    return { name: option.name, value: option.id, image: option.image };
+  });
+
+  return (
+    <Container>
+      <ConverterValueDiv>
+        <SelectWithSearch
+          options={mappedOptions}
+          coin={fromCoin}
+          setCoin={setfromCoin}
+        />
+      </ConverterValueDiv>
+      <ConverterValueDiv>
+        <SelectWithSearch
+          options={mappedOptions}
+          coin={toCoin}
+          setCoin={setToCoin}
+        />
+      </ConverterValueDiv>
+    </Container>
+  );
 }
