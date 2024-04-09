f894bde2d39fed10cc1286a5a4f36193c14b7b1d
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-09 10:54:37 -0400

    move-styled-components-for-SliderButton-to-separate-file

--- src/app/components/Converter/SliderButton.tsx
+++ src/app/components/Converter/SliderButton.tsx
@@ -1,30 +1,8 @@
-import Link from "next/link";
 import { usePathname } from "next/navigation";
-import styled from "styled-components";
-
-const LinkWrapper = styled.div`
-  width: 40%;
-  margin: 20px 100px;
-  background-color: ${({theme}) => theme.sliderButton.containerBackground};
-  display: flex;
-  border-radius: 6px;
-`;
-
-const StyledLink = styled(Link)`
-  padding: 12px 32px;
-  background-color: ${({theme}) => theme.sliderButton.backgroundColor};
-  text-align: center;
-  width: 50%;
-  border-radius: 6px;
-  color: ${({theme}) => theme.sliderButton.inactiveTextColor};
-  font-size: 16px;
-  text-decoration: none;
-  &.active {
-    background-color: ${({theme}) => theme.sliderButton.activeBackgroundColor};
-    font-weight: 500;
-    color: ${({theme}) => theme.sliderButton.activeTextColor}
-  }
-`;
+import {
+  LinkWrapper,
+  StyledLink,
+} from "@/app/styling/components/Converter/styled.SliderButton";
 
 const SliderButton = () => {
   const pathname = usePathname();
