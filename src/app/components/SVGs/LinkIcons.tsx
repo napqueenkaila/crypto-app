aaf0a6b90eea33c6d49d68e2043d0dc57ae1d11f
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-09 11:17:03 -0400

    use-darkMode-prop-to-dynamically-render-color

--- src/app/components/SVGs/LinkIcons.tsx
+++ src/app/components/SVGs/LinkIcons.tsx
@@ -1,4 +1,4 @@
-export const LinkIcon = () => {
+export const LinkIcon = ({darkMode}:{darkMode: boolean}) => {
   return (
     <svg
       width="21"
@@ -9,14 +9,14 @@ export const LinkIcon = () => {
     >
       <path
         d="M11.5375 9.11621C13.4125 10.9912 13.4125 14.0245 11.5375 15.8912C9.6625 17.7579 6.62917 17.7662 4.7625 15.8912C2.89584 14.0162 2.8875 10.9829 4.7625 9.11621"
-        stroke="white"
+        stroke={darkMode ? "#FFFFFF" : "#6161D680"}
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M9.47812 11.1754C7.52812 9.22539 7.52812 6.05873 9.47812 4.10039C11.4281 2.14206 14.5948 2.15039 16.5531 4.10039C18.5115 6.05039 18.5031 9.21706 16.5531 11.1754"
-        stroke="white"
+        stroke={darkMode ? "#FFFFFF" : "#6161D680"}
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
@@ -26,8 +26,10 @@ export const LinkIcon = () => {
 };
 
 export const CopyIcon = ({
+  darkMode,
   copyToClipboard,
 }: {
+  darkMode: boolean,
   copyToClipboard: () => void;
 }) => {
   return (
@@ -41,14 +43,14 @@ export const CopyIcon = ({
     >
       <path
         d="M13.987 10.7503V14.2503C13.987 17.167 12.8203 18.3337 9.90365 18.3337H6.40365C3.48698 18.3337 2.32031 17.167 2.32031 14.2503V10.7503C2.32031 7.83366 3.48698 6.66699 6.40365 6.66699H9.90365C12.8203 6.66699 13.987 7.83366 13.987 10.7503Z"
-        stroke="white"
+        stroke={darkMode ? "#FFFFFF" : "#6161D680"}
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M18.987 5.75033V9.25033C18.987 12.167 17.8203 13.3337 14.9036 13.3337H13.987V10.7503C13.987 7.83366 12.8203 6.66699 9.90365 6.66699H7.32031V5.75033C7.32031 2.83366 8.48698 1.66699 11.4036 1.66699H14.9036C17.8203 1.66699 18.987 2.83366 18.987 5.75033Z"
-        stroke="white"
+        stroke={darkMode ? "#FFFFFF" : "#6161D680"}
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
