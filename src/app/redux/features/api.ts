2e6b4966f9bf4aaf633e7c3f59a70b7ce5c696d9
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-12 10:45:57 -0400

    pass-coinOneSelected-to-getChartData-in-api.ts

--- src/app/redux/features/api.ts
+++ src/app/redux/features/api.ts
@@ -88,8 +88,8 @@ export const api = createApi({
       query: (searchQuery) => `search?query=${searchQuery}`,
     }),
     getChartData: builder.query({
-      query: ({ currency, defaultCoin, selectedDays }) =>
-        `coins/${defaultCoin}/market_chart?vs_currency=${currency}&days=${selectedDays}&interval=daily`,
+      query: ({ currency, coinOneSelected, selectedDays }) =>
+        `coins/${coinOneSelected}/market_chart?vs_currency=${currency}&days=${selectedDays}&interval=daily`,
     }),
     getTableData: builder.query({
       query: ({ page, currency }) =>
