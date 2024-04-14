6d74b48035577d8e273f818a1a739b798a56e858
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-14 12:29:20 -0400

    add-compareCoins-to-selectedCoinsSlice.ts-and-change-state-in-setCoin-reducers

--- src/app/redux/features/selectedCoinsSlice.ts
+++ src/app/redux/features/selectedCoinsSlice.ts
@@ -3,11 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 interface SelectedCoinsState {
   coinOne: { id: string; name: string; symbol: string };
   coinTwo: { id: string; name: string; symbol: string } | null;
+  compareCoins: boolean;
 }
 
 const initialState: SelectedCoinsState = {
   coinOne: { id: "bitcoin", name: "Bitcoin", symbol: "btc" },
   coinTwo: null,
+  compareCoins: false,
 };
 
 const selectedCoinsSlice = createSlice({
@@ -16,15 +18,23 @@ const selectedCoinsSlice = createSlice({
   reducers: {
     setCoinOne: (state, action) => {
       state.coinOne = action.payload;
+      if (state.coinOne && state.coinTwo) {
+        state.compareCoins = true;
+      }
     },
     setCoinTwo: (state, action) => {
       state.coinTwo = action.payload;
+      if (state.coinOne && state.coinTwo) {
+        state.compareCoins = true;
+      }
     },
   },
 });
 
-export const { setCoinOne } = selectedCoinsSlice.actions;
+export const { setCoinOne, setCoinTwo } = selectedCoinsSlice.actions;
 
 export const selectCoinOne = (state: any) => state.selectedCoins.coinOne;
 export const selectCoinTwo = (state: any) => state.selectedCoins.coinTwo;
+export const selectCompareCoins = (state: any) =>
+  state.selectedCoins.compareCoins;
 export default selectedCoinsSlice.reducer;
