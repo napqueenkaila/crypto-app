b2bd3a05974bc5477fd7de0163ffe1ec56fef68f
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-15 13:33:48 -0400

    write-formatChartData-function-in-utils.ts

--- src/app/components/Charts/utils.ts
+++ src/app/components/Charts/utils.ts
@@ -1,5 +1,8 @@
 export const formatDateLabel = (date: number) => {
-    return new Intl.DateTimeFormat("en", { day: "2-digit", month: "short" }).format(date);
+  return new Intl.DateTimeFormat("en", {
+    day: "2-digit",
+    month: "short",
+  }).format(date);
 };
 
 export const todaysDate = new Intl.DateTimeFormat("en-US", {
@@ -7,3 +10,7 @@ export const todaysDate = new Intl.DateTimeFormat("en-US", {
   month: "long",
   day: "numeric",
 }).format(new Date(Date.now()));
+
+export const formatChartData = (data: number[][]) => {
+  return data.map((el) => el[1]);
+};
