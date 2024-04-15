228711f139d1230c5b948e22b56893eb2ea6917d
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-15 13:17:29 -0400

    use-skip-arg-in-query-for-second-coin-and-update-skip-in-useEffect

--- src/app/components/Charts/ChartsContainer.tsx
+++ src/app/components/Charts/ChartsContainer.tsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAppSelector } from "@/app/redux/hooks";
 import { selectCurrency } from "@/app/redux/features/currencySlice";
-import { selectCoinOne } from "@/app/redux/features/selectedCoinsSlice";
+import {
+  selectCoinOne,
+  selectCoinTwo,
+} from "@/app/redux/features/selectedCoinsSlice";
 import { useGetChartDataQuery } from "@/app/redux/features/api";
 import RangeBar from "./RangeBar";
 import LineChart from "./LineChart";
@@ -17,14 +20,28 @@ const Container = styled.div`
 `;
 
 const ChartsContainer = () => {
+  const [skip, setSkip] = useState(true);
   const { currency } = useAppSelector(selectCurrency);
   const coinOneSelected = useAppSelector(selectCoinOne);
+  const coinTwoSelected = useAppSelector(selectCoinTwo);
   const [selectedDays, setSelectedDays] = useState(7);
   const { data: coinOneData, isSuccess } = useGetChartDataQuery({
     currency,
     selectedCoinId: coinOneSelected.id,
     selectedDays,
   });
+  const { data: coinTwoData } = useGetChartDataQuery(
+    { currency, selectedCoinId: coinTwoSelected.id, selectedDays },
+    { skip }
+  );
+
+  useEffect(() => {
+    if (coinTwoSelected.id === "") {
+      setSkip(true);
+    } else if (coinTwoSelected.id !== "") {
+      setSkip(false);
+    }
+  }, [coinTwoSelected]);
 
   const handleDaysChange = (e) => {
     const newDays = Number(e.target.value);
