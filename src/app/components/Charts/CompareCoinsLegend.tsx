d224a2348e91c851970d5a34a8886e5c0d6057cf
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-17 16:56:27 -0400

    create-CompareCoinsLegend.tsx

---
+++ src/app/components/Charts/CompareCoinsLegend.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const CoinContainer = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+
+const ColorBox = styled.div`
+  width: 24px;
+  height: 24px;
+  background-color: ${(props) => props.color};
+`;
+
+const CompareCoinsLegend = ({
+  coinOne,
+  coinTwo,
+}: {
+  coinOne: string;
+  coinTwo: string;
+}) => {
+  return (
+    <Container>
+      <CoinContainer>
+        <ColorBox color="#7878fa" />
+        <div>{coinOne}</div>
+      </CoinContainer>
+      <CoinContainer>
+        <ColorBox color="#D878FA" />
+        <div>{coinTwo}</div>
+      </CoinContainer>
+    </Container>
+  );
+};
+export default CompareCoinsLegend;
