5ad5d50142a146f214a7bf7cd2fdfdd0ded351a3
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-17 12:11:48 -0400

    write-getChartBackgroundColor-function-and-define-chartColors-object

--- src/app/components/Charts/utils.ts
+++ src/app/components/Charts/utils.ts
@@ -16,3 +16,22 @@ export const todaysDate = new Intl.DateTimeFormat("en-US", {
 export const formatChartData = (data: number[][]) => {
   return data?.map((el) => el[1]);
 };
+
+interface ChartColors {
+  one: { [key: string]: string };
+  two: { [key: string]: string };
+}
+
+const chartColors: ChartColors = {
+  one: { start: "rgba(116,116,242,0.6) ", end: "rgba(116,116,242,0.01)" },
+  two: { start: "rgba(231,114,255,0.6) ", end: "rgba(231,114,255,0.01)" },
+};
+
+export const getChartBackgroundColor =
+  (type: string) => (context: { chart: { ctx: any } }) => {
+    const ctx = context.chart.ctx;
+    const gradient = ctx.createLinearGradient(0, 0, 0, 350);
+    gradient.addColorStop(0, chartColors[type as keyof ChartColors].start);
+    gradient.addColorStop(1, chartColors[type as keyof ChartColors].end);
+    return gradient;
+  };
