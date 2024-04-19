559ec8014a94e094bce4ac5ad50799bdbb1e9abc
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-19 15:40:36 -0400

    create-separate-options-objects-for-each-chart

--- src/app/components/Charts/options.ts
+++ src/app/components/Charts/options.ts
@@ -1,4 +1,50 @@
-export const options = {
+export const lineOptions = {
+  onHover: {} as any,
+  responsive: true,
+  maintainAspectRatio: false,
+  interaction: {
+    intersect: false,
+    mode: "x" as "x",
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+    crosshair: {
+      line: { color: "#FFF", width: 1 },
+      sync: { enabled: false },
+    },
+    tooltip: {
+      backgroundColor: "rgba(0,0,0,0)",
+      borderWidth: 0.3,
+      intersect: false,
+    },
+  },
+  scales: {
+    y: {
+      display: false,
+    },
+    x: {
+      display: true,
+      ticks: {
+        color: "#9b9ab6",
+      },
+      grid: {
+        display: false,
+      },
+    },
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+    line: {
+      tension: 0.5,
+    },
+  },
+};
+
+export const barOptions = {
   onHover: {} as any,
   responsive: true,
   maintainAspectRatio: false,
@@ -36,9 +82,6 @@ export const options = {
     },
   },
   elements: {
-    point: {
-      radius: 0,
-    },
     line: {
       tension: 0.5,
     },
