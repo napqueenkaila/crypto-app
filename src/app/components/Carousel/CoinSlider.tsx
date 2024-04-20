b6f21c0128d11047bd2c74c3e92301d41ebae000
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-20 15:04:33 -0400

    add-responsive-breakpoints-for-slider-settings

--- src/app/components/Carousel/CoinSlider.tsx
+++ src/app/components/Carousel/CoinSlider.tsx
@@ -13,8 +13,23 @@ const CoinSlider = () => {
   const settings = {
     dots: false,
     speed: 500,
+    infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ]
   };
 
   return (
