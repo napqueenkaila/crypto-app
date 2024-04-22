0d85374d22cbc3eec72f903e8a75b28dbd8e56ea
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-22 10:25:49 -0400

    add-DownArrow.tsx-to-SVGs-folder

---
+++ src/app/components/SVGs/DownArrow.tsx
@@ -0,0 +1,16 @@
+export const DownArrow = () => {
+  return (
+    <svg
+      width="8"
+      height="4"
+      viewBox="0 0 8 4"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3.99935 3.66699L7.33268 0.333659L0.666016 0.333659L3.99935 3.66699Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
