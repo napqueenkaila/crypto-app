0e11e456a301ae6acf885333b67fb0b5f070ad88
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-22 09:32:41 -0400

    useRef-to-handleOutsideClick-when-click-outside-DropdownDiv

--- src/app/components/Navbar/Search.tsx
+++ src/app/components/Navbar/Search.tsx
@@ -1,32 +1,54 @@
-import { SetStateAction, useState } from "react";
+import { SetStateAction, useEffect, useRef, useState } from "react";
 import { useGetSearchDataQuery } from "@/app/redux/features/api";
-import { SearchInput, DropdownDiv, DropdownItem } from "@/app/styling/components/Navbar/styled.navbar";
+import {
+  SearchInput,
+  DropdownDiv,
+  DropdownItem,
+} from "@/app/styling/components/Navbar/styled.navbar";
 
 const Search = () => {
-    const [searchQuery, setSearchQuery] = useState("");
-    const { data } = useGetSearchDataQuery(searchQuery);
-    const dropdownCoins = data?.coins.slice(0, 5);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const { data } = useGetSearchDataQuery(searchQuery);
+  const dropdownCoins = data?.coins.slice(0, 5);
 
-    const handleSearch = (e: { target: { value: SetStateAction<string> } }) => {
-      setSearchQuery(e.target.value);
+  const handleSearch = (e: { target: { value: SetStateAction<string> } }) => {
+    setIsOpen(true);
+    setSearchQuery(e.target.value);
+  };
+
+  const handleOutsideClick = (e) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
     };
-    return (
-      <div>
-        <SearchInput
-          type="text"
-          placeholder="&#128270;  Search..."
-          onChange={handleSearch}
-        />
-        <DropdownDiv>
-          {dropdownCoins?.map((coin: { id: string; name: string }) => {
+  }, []);
+
+  return (
+    <div>
+      <SearchInput
+        type="text"
+        placeholder="&#128270;  Search..."
+        onChange={handleSearch}
+      />
+      <DropdownDiv ref={dropdownRef}>
+        {isOpen &&
+          dropdownCoins?.map((coin: { id: string; name: string }) => {
             return (
               <DropdownItem href={`/coin/${coin.id}`} key={coin.id}>
                 {coin.name}
               </DropdownItem>
             );
           })}
-        </DropdownDiv>
-      </div>
-    );
+      </DropdownDiv>
+    </div>
+  );
 };
 export default Search;
