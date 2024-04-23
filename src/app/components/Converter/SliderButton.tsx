ea01ae23a1c916d7e9e3b31ee8ecbf9e53da0c41
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-23 12:22:21 -0400

    usePrefetch-to-fetch-InitialConverterCoins-onMouseEnter

--- src/app/components/Converter/SliderButton.tsx
+++ src/app/components/Converter/SliderButton.tsx
@@ -3,9 +3,14 @@ import {
   LinkWrapper,
   StyledLink,
 } from "@/app/styling/components/Converter/styled.SliderButton";
+import { usePrefetch } from "@/app/redux/features/api";
+import { useAppSelector } from "@/app/redux/hooks";
+import { selectCurrency } from "@/app/redux/features/currencySlice";
 
 const SliderButton = () => {
   const pathname = usePathname();
+  const { currency } = useAppSelector(selectCurrency);
+  const prefetchInitialCoins = usePrefetch("getInitialConverterCoins");
 
   return (
     <LinkWrapper>
@@ -15,6 +20,7 @@ const SliderButton = () => {
       <StyledLink
         href="/converter"
         className={`${pathname === "/converter" ? "active" : ""} `}
+        onMouseEnter={() => prefetchInitialCoins(currency, { force: true })}
       >
         Converter
       </StyledLink>
