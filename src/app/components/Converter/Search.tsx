3f12d6f722c5c27a25d7d65de0148947c296c37f
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-25 10:03:30 -0400

    call-useGetSearchDataQuery-and-create-div-to-render-search-results

--- src/app/components/Converter/Search.tsx
+++ src/app/components/Converter/Search.tsx
@@ -1,9 +1,12 @@
+import { useGetSearchDataQuery } from "@/app/redux/features/api";
 import { SetStateAction, useState } from "react";
 
 const Search = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const { data } = useGetSearchDataQuery(searchQuery);
+  const dropDownCoins = data.coins.slice(0, 10);
 
-  const handleSearch = (e: { target: { value: SetStateAction<string>; }; }) => {
+  const handleSearch = (e: { target: { value: SetStateAction<string> } }) => {
     setSearchQuery(e.target.value);
   };
 
@@ -15,6 +18,11 @@ const Search = () => {
         value={searchQuery}
         onChange={handleSearch}
       />
+      <div>
+        {dropDownCoins?.map((coin: { id: string; name: string }) => {
+          return <div key={coin.id}>{coin.name}</div>;
+        })}
+      </div>
     </>
   );
 };
