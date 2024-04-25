6c10305c360fe0ec84f5c552ebaa78520ad60880
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-25 08:59:23 -0400

    delete-SelectWithSearch.tsx-and-update-page.tsx

--- src/app/converter/page.tsx
+++ src/app/converter/page.tsx
@@ -1,10 +1,5 @@
 "use client";
-import { useState } from "react";
 import styled from "styled-components";
-import { useAppSelector } from "../redux/hooks";
-import { selectCurrency } from "../redux/features/currencySlice";
-import { useGetCarouselDataQuery } from "../redux/features/api";
-import SelectWithSearch from "../components/Converter/SelectWithSearch";
 
 const Container = styled.div`
   width: 100%;
@@ -18,32 +13,14 @@ const ConverterValueDiv = styled.div`
 `;
 
 export default function Converter() {
-  const { currency } = useAppSelector(selectCurrency);
-  const [fromCoin, setFromCoin] = useState("bitcoin");
-  const [toCoin, setToCoin] = useState("ethereum");
-  const { data: options } = useGetCarouselDataQuery(currency);
 
-  const mappedOptions = options?.map((option) => ({
-    name: option.name,
-    value: option.id,
-    image: option.image,
-  }));
 
   return (
     <Container>
       <ConverterValueDiv>
-        <SelectWithSearch
-          options={mappedOptions}
-          coin={fromCoin}
-          setCoin={setFromCoin}
-        />
       </ConverterValueDiv>
       <ConverterValueDiv>
-        <SelectWithSearch
-          options={mappedOptions}
-          coin={toCoin}
-          setCoin={setToCoin}
-        />
+
       </ConverterValueDiv>
     </Container>
   );
