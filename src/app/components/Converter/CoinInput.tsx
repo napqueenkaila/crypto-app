3653a81cf1a5d2d89f1001995674c89e2e1c3382
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-25 09:57:40 -0400

    import-Search-component-and-useState-to-toggle-isSearching

--- src/app/components/Converter/CoinInput.tsx
+++ src/app/components/Converter/CoinInput.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { DownArrow } from "../SVGs/DownArrow";
+import Search from "./Search";
+import { useState } from "react";
 
 interface Coin {
   name: string;
@@ -9,12 +11,23 @@ interface Coin {
 }
 
 const CoinInput = ({ coin }: { coin: Coin }) => {
+  const [isSearching, setIsSearching] = useState(false);
 
+  const handleShowSearch = () => { 
+    setIsSearching(true);
+  };
   return (
-    <div>
-      <Image src={coin.image} alt="" width={25} height={25} />
-      {coin.name}
-      <DownArrow />
+    <div onClick={handleShowSearch}>
+      {isSearching ? (
+        <Search />
+      ) : (
+        <>
+          <Image src={coin.image} alt="" width={25} height={25} />
+          <div> {coin.name}</div>
+          <DownArrow />
+        </>
+      )}
+      <div></div>
     </div>
   );
 };
