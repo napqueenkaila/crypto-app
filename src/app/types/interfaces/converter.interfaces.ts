a67712ff44e4410ee7227248d66de0c7eb4fab12
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-29 15:46:05 -0400

    create-converter.interfaces.ts-and-move-component-prop-interfaces

---
+++ src/app/types/interfaces/converter.interfaces.ts
@@ -0,0 +1,24 @@
+import { Dispatch, SetStateAction } from "react";
+import { TableData } from "./api.interfaces";
+
+export interface CoinInputProps {
+  coinData: TableData;
+  setCoin: Dispatch<SetStateAction<TableData>>;
+  quantity: number;
+  resetQuantities: () => void;
+  handleQuantityChange: (e: any) => void;
+}
+
+export interface CoinsContainerProps {
+  fromCoin: TableData;
+  toCoin: TableData;
+  setFromCoin: Dispatch<SetStateAction<TableData>>;
+  setToCoin: Dispatch<SetStateAction<TableData>>;
+}
+
+export interface SearchProps {
+  setCoin: Dispatch<SetStateAction<TableData>>;
+  resetQuantities: () => void;
+  isSearching: boolean;
+  setIsSearching: Dispatch<SetStateAction<boolean>>;
+}
