5862d2b3eca52edea61bf17814704a3d61f0b277
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-29 14:52:23 -0400

    move-interfaces-in-api.ts-to-types/interfaces/api.interfaces.ts

--- src/app/redux/features/api.ts
+++ src/app/redux/features/api.ts
@@ -1,61 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { toggleHasMore } from "./hasMoreSlice";
-
-interface MarketData {
-  active_cryptocurrencies: number;
-  markets: number;
-  total_volume: { [key: string]: number };
-  total_market_cap: { [key: string]: number };
-  market_cap_percentage: { [key: string]: number };
-}
-
-interface TableData {
-  id: string;
-  name: string;
-  symbol: string;
-  image: string;
-  market_cap_rank: number;
-  current_price: number;
-  price_change_percentage_1h_in_currency: number;
-  price_change_percentage_24h_in_currency: number;
-  price_change_percentage_7d_in_currency: number;
-  market_cap: number;
-  total_volume: number;
-  circulating_supply: number;
-  total_supply: number;
-  sparkline_in_7d: { price: number[] };
-}
-
-interface CoinData {
-  id: string;
-  name: string;
-  symbol: string;
-  image: { thumb: string };
-  links: { homepage: string[]; blockchain_site: string[] };
-  description: { en: string };
-  market_data: {
-    ath: Record<string, number>;
-    ath_date: Record<string, string>;
-    atl: Record<string, number>;
-    atl_date: Record<string, string>;
-    market_cap: Record<string, number>;
-    fully_diluted_valuation: Record<string, number>;
-    total_volume: Record<string, number>;
-    total_supply: number;
-    circulating_supply: number;
-    max_supply: number;
-    current_price: Record<string, number>;
-  };
-}
-
-interface CarouselData {
-  id: string;
-  name: string;
-  symbol: string;
-  image: string;
-  current_price: number;
-  price_change_percentage_24h: number;
-}
+import {
+  MarketData,
+  TableData,
+  CoinData,
+  CarouselData,
+} from "@/app/types/interfaces/api.interfaces";
 
 const apiKey: string = process.env.NEXT_PUBLIC_API_KEY!;
 
@@ -88,7 +38,15 @@ export const api = createApi({
       query: (searchQuery) => `search?query=${searchQuery}`,
     }),
     getChartData: builder.query({
-      query: ({ currency, selectedCoinId, selectedDays }: {currency: string, selectedCoinId: string, selectedDays: number}) =>
+      query: ({
+        currency,
+        selectedCoinId,
+        selectedDays,
+      }: {
+        currency: string;
+        selectedCoinId: string;
+        selectedDays: number;
+      }) =>
         `coins/${selectedCoinId}/market_chart?vs_currency=${currency}&days=${selectedDays}&interval=daily`,
     }),
     getTableData: builder.query({
