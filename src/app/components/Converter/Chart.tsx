6820946908a8eb915bf20e190fdf4d23f5ad5045
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-30 10:56:28 -0400

    pass-down-props-and-define-chartLabels-and-datasets

--- src/app/components/Converter/Chart.tsx
+++ src/app/components/Converter/Chart.tsx
@@ -8,6 +8,14 @@ import {
   Tooltip,
   Filler,
 } from "chart.js";
+import { Line } from "react-chartjs-2";
+import { TableData } from "@/app/redux/features/api";
+import {
+  formatChartData,
+  getChartGradient,
+  getChartLabels,
+} from "../Charts/utils";
+import { lineOptions } from "../Charts/options";
 
 ChartJS.register(
   CategoryScale,
@@ -19,8 +27,45 @@ ChartJS.register(
   Filler
 );
 
-const Chart = () => {
-  return <div>chart</div>;
+const Chart = ({
+  fromCoin,
+  fromCoinData,
+  toCoin,
+  toCoinData,
+}: {
+  fromCoin: TableData;
+  fromCoinData: number[][];
+  toCoin: TableData;
+  toCoinData: number[][];
+}) => {
+  const chartLabels = getChartLabels(fromCoinData);
+  const datasets = [
+    {
+      label: fromCoin.name,
+      data: formatChartData(fromCoinData),
+      borderColor: "#7878fa",
+      fill: true,
+      borderWidth: 2,
+      backgroundColor: getChartGradient("one"),
+    },
+    {
+      label: toCoin.name,
+      data: formatChartData(toCoinData),
+      borderColor: "#D878FA",
+      fill: true,
+      borderWidth: 2,
+      backgroundColor: getChartGradient("two"),
+    },
+  ];
+
+  return (
+    <div>
+      <Line
+        options={lineOptions}
+        data={{ labels: chartLabels, datasets: datasets }}
+      />
+    </div>
+  );
 };
 
 export default Chart;
