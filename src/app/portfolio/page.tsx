fe2e32ed9c8f663dab270240caac5612761ca363
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-30 13:01:41 -0400

    implement-button-to-show-AddAssetModal

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import styled from "styled-components";
+import AddAssetModal from "../components/Portfolio/AddAsset/AddAssetModal";
+import { useState } from "react";
+
+const PageWrapper = styled.div`
+  width: 90vw;
+  margin: 0 auto;
+`;
+
 export default function Portfolio() {
-  return <div>Portfolio Page</div>;
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <PageWrapper>
+      <div>
+        <div>Portfolio</div>
+        <button onClick={() => setShowModal(true)}>Add Asset</button>
+      </div>
+      {showModal && <AddAssetModal setShowModal={setShowModal} />}
+    </PageWrapper>
+  );
 }
