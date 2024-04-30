29163f4c941fcf0bc1163a3d35da7602cda635e6
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-04-30 10:26:19 -0400

    create-Chart.tsx-for-converter-page

---
+++ src/app/components/Converter/Chart.tsx
@@ -0,0 +1,26 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler
+);
+
+const Chart = () => {
+  return <div>chart</div>;
+};
+
+export default Chart;
