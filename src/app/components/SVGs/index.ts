482d8f10988678f2b93705bdd5a6db66cd80f8c9
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-05-10 10:51:15 -0400

    create-EditIcon.tsx

--- src/app/components/SVGs/index.ts
+++ src/app/components/SVGs/index.ts
@@ -1,4 +1,4 @@
-import { BtcIcon, EthIcon } from "./NavIcons/CoinIcons";
+import { BtcIcon, EthIcon, CryptoIcon } from "./NavIcons/CoinIcons";
 import { DarkModeIcon } from "./NavIcons/DarkModeIcon";
 import { DollarIcon } from "./NavIcons/DollarIcon";
 import { HomeIcon } from "./NavIcons/HomeIcon";
@@ -9,5 +9,23 @@ import { ArrowIcon } from "./ArrowIcon";
 import { CopyIcon, LinkIcon } from "./LinkIcons";
 import { SwapIcon } from "./SwapIcon";
 import { CloseCircle } from "./CloseCircle";
+import EditIcon from "./EditIcon";
 
-export { BtcIcon, EthIcon, DarkModeIcon, DollarIcon, HomeIcon, Logo, ExchangeIcon, FlashIcon, PortfolioIcon, ArrowIcon, CopyIcon, LinkIcon, SwapIcon, CloseCircle };
+export {
+  BtcIcon,
+  EthIcon,
+  CryptoIcon,
+  DarkModeIcon,
+  DollarIcon,
+  HomeIcon,
+  Logo,
+  ExchangeIcon,
+  FlashIcon,
+  PortfolioIcon,
+  ArrowIcon,
+  CopyIcon,
+  LinkIcon,
+  SwapIcon,
+  CloseCircle,
+  EditIcon,
+};
