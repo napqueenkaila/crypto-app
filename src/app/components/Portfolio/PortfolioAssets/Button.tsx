f88e622721ea1343f8adc8125fe963431f6503f3
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-05-13 16:45:44 -0400

    add-styles-to-Button.tsx

--- src/app/components/Portfolio/PortfolioAssets/Button.tsx
+++ src/app/components/Portfolio/PortfolioAssets/Button.tsx
@@ -1,4 +1,16 @@
+import styled from "styled-components";
 import { EditIcon, TrashIcon } from "../../SVGs";
+
+const StyledButton = styled.div`
+  background-color: #3a3978;
+  padding: 8px;
+  border-radius: 4px;
+  border: none;
+  display: flex;
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 const Button = ({
   handleClick,
@@ -10,12 +22,13 @@ const Button = ({
   type: string;
 }) => {
   return (
-    <button
-      onClick={() => handleClick(id)}
-      style={{ backgroundColor: "black" }}
-    >
-      {type === "trash" ? <TrashIcon /> : <EditIcon />}
-    </button>
+    <StyledButton>
+      {type === "trash" ? (
+        <TrashIcon handleClick={handleClick} id={id} />
+      ) : (
+        <EditIcon handleClick={handleClick} id={id} />
+      )}
+    </StyledButton>
   );
 };
 export default Button;
