ebb03493abc2ea2f96919a3984eab5799d25c7dd
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-05-13 16:46:51 -0400

    add-styles-to-DataBlock.tsx

--- src/app/components/Portfolio/PortfolioAssets/DataBlock.tsx
+++ src/app/components/Portfolio/PortfolioAssets/DataBlock.tsx
@@ -1,6 +1,24 @@
 import styled from "styled-components";
 import { ArrowIcon } from "../../SVGs";
 
+const DataContainer = styled.div`
+  border: 1px solid #2d2d51;
+  padding: 15px 10px;
+  border-radius: 8px;
+  background-color: #191925;
+`;
+
+const DataBlockTitle = styled.div`
+  color: #d1d1d1;
+  font-size: 14px;
+`;
+
+const Data = styled.div<{ $color: string }>`
+  font-size: 20px;
+  font-weight: 500;
+  color: ${(props) => props.$color}
+`;
+
 const PercentageBar = styled.div`
   width: 250px;
   height: 6px;
@@ -25,24 +43,34 @@ const DataBlock = ({
   isPercent: boolean;
   hasPercentBar: boolean;
 }) => {
+  const getColor = () => {
+    if (!isPercent) {
+      return "#FFFFFF";
+    } else if (isPercent && Number(coinData) >= 0) {
+      return "#01f1e3";
+    } else {
+      return "#FE2264";
+    }
+  };
+
   return (
-    <div>
+    <DataContainer>
       <div>
-        <div>
+        <Data $color={getColor()}>
           {isPercent && !hasPercentBar && (
             <ArrowIcon isPositive={Number(coinData) > 0 ? true : false} />
           )}
           {Number(coinData) < 0 ? Number(coinData) * -1 : coinData}
           {isPercent && "%"}
-        </div>
+        </Data>
         {hasPercentBar && (
           <PercentageBar>
             <Percent $percent={Number(coinData)} />
           </PercentageBar>
         )}
       </div>
-      <div>{title}</div>
-    </div>
+      <DataBlockTitle>{title}</DataBlockTitle>
+    </DataContainer>
   );
 };
 export default DataBlock;
