5dd766241089399a388bc64917fc8ef2596746f6
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-05-13 17:37:22 -0400

    add-styles-to-ModalHeader.tsx

--- src/app/components/Portfolio/AddAsset/ModalHeader.tsx
+++ src/app/components/Portfolio/AddAsset/ModalHeader.tsx
@@ -1,5 +1,13 @@
 import { Dispatch, SetStateAction } from "react";
 import { CloseCircle } from "../../SVGs";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 20px;
+  grid-area: header;
+`;
 
 const ModalHeader = ({
   setShowModal,
@@ -7,10 +15,10 @@ const ModalHeader = ({
   setShowModal: Dispatch<SetStateAction<boolean>>;
 }) => {
   return (
-    <>
+    <HeaderContainer>
       <div>Select coins</div>
       <CloseCircle setShowModal={setShowModal} />
-    </>
+    </HeaderContainer>
   );
 };
 export default ModalHeader;
