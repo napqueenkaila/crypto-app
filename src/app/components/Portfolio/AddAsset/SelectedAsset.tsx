d9606e3d5b5e12b75f689de13bd2d7ff026e2fb5
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-05-13 17:37:52 -0400

    add-styles-to-SelectedAsset.tsx

--- src/app/components/Portfolio/AddAsset/SelectedAsset.tsx
+++ src/app/components/Portfolio/AddAsset/SelectedAsset.tsx
@@ -1,21 +1,47 @@
 import Image from "next/image";
 import { CryptoIcon } from "../../SVGs/NavIcons/CoinIcons";
 import { SelectedCoin } from "./AddAssetModal";
+import styled from "styled-components";
+
+const SelectedContainer = styled.div`
+  grid-area: selected;
+  background-color: #191932;
+  border-radius: 8px;
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+`;
+
+const IconDiv = styled.div`
+  background-color: #2c2c4a;
+  padding: 16px;
+  border-radius: 8px;
+  width: 64px;
+  height: 64px;
+`;
+
+const SelectedTitle = styled.div`
+  font-size: 28px;
+  font-weight: 700;
+`;
 
 const SelectedAsset = ({ selectedCoin }: { selectedCoin: SelectedCoin }) => {
   return (
-    <div>
-      {selectedCoin.large === undefined ? (
-        <CryptoIcon />
-      ) : (
-        <Image src={selectedCoin.large} alt="" height={25} width={25} />
-      )}
-      {selectedCoin.name === undefined ? (
-        <div>Select a Coin</div>
-      ) : (
-        <div>{selectedCoin.name}</div>
-      )}
-    </div>
+    <SelectedContainer>
+      <IconDiv>
+        {selectedCoin.large === undefined ? (
+          <CryptoIcon />
+        ) : (
+          <Image src={selectedCoin.large} alt="" height={25} width={25} />
+        )}
+      </IconDiv>
+      <SelectedTitle>
+        {selectedCoin.name === undefined ? "Select a Coin" : selectedCoin.name}
+      </SelectedTitle>
+    </SelectedContainer>
   );
 };
 export default SelectedAsset;
