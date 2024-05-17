ecf305df0047491e5f2e5b0c0bae6f0b46595615
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-05-17 10:37:47 -0400

    style-SwapIcon-based-on-darkMode

--- src/app/components/SVGs/SwapIcon.tsx
+++ src/app/components/SVGs/SwapIcon.tsx
@@ -1,6 +1,9 @@
+import { useAppSelector } from "@/app/redux/hooks";
+import { selectDarkMode } from "@/app/redux/features/darkModeSlice";
 import { StyledSVGContainer } from "@/app/styling/components/SVGs/styled.SVG";
 
 export const SwapIcon = ({ onClick }: { onClick: () => void }) => {
+  const { darkMode } = useAppSelector(selectDarkMode);
   return (
     <StyledSVGContainer>
       <svg
@@ -11,17 +14,22 @@ export const SwapIcon = ({ onClick }: { onClick: () => void }) => {
         xmlns="http://www.w3.org/2000/svg"
         onClick={onClick}
       >
-        <rect width="48" height="48" rx="24" fill="white" />
+        <rect
+          width="48"
+          height="48"
+          rx="24"
+          fill={darkMode ? "#FFFFFF" : "#353570"}
+        />
         <path
           d="M14.5 28L18.5 32M18.5 32L22.5 28M18.5 32L18.5 18C18.5 16.8954 19.3954 16 20.5 16V16"
-          stroke="#3D3D7E"
+          stroke={darkMode ? "#3D3D7E" : "#FFFFFF"}
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
         <path
           d="M25.5 20L29.5 16M29.5 16L33.5 20M29.5 16L29.5 31C29.5 32.1046 28.6046 33 27.5 33V33"
-          stroke="#3D3D7E"
+          stroke={darkMode ? "#3D3D7E" : "#FFFFFF"}
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
