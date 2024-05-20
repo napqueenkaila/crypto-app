16717d4f7ef8d3e0ff91c39f2fc2744acbfa5275
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2024-05-20 17:37:53 -0400

    handle-reset-assetToEdit-onClick-modal-close

--- src/app/components/Portfolio/AddAsset/ModalHeader.tsx
+++ src/app/components/Portfolio/AddAsset/ModalHeader.tsx
@@ -1,15 +1,28 @@
 import { Dispatch, SetStateAction } from "react";
 import { CloseCircle } from "../../SVGs";
+import { FormDataState } from "./AddAssetModal";
 
 const ModalHeader = ({
   setShowModal,
+  setAssetToEdit,
 }: {
   setShowModal: Dispatch<SetStateAction<boolean>>;
+  setAssetToEdit: Dispatch<SetStateAction<FormDataState>>;
 }) => {
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setAssetToEdit({
+      id: "",
+      selectedCoin: {},
+      selectedAmount: "",
+      selectedDate: "",
+    });
+  };
+
   return (
     <>
       <div>Select coins</div>
-      <CloseCircle setShowModal={setShowModal} />
+      <CloseCircle handleCloseModal={handleCloseModal} />
     </>
   );
 };
